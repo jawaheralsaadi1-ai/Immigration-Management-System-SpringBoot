e13eee0173b58bf2b2551f2730e913a4edb6a6ea
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 16:22:28 +0400

    add(VisaApplicationRepository): implement two  custom query methods.

--- src/main/java/com/immigration/system/repository/VisaApplicationRepository.java
+++ src/main/java/com/immigration/system/repository/VisaApplicationRepository.java
@@ -3,5 +3,10 @@ package com.immigration.system.repository;
 import com.immigration.system.entities.VisaApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public  interface VisaApplicationRepository extends JpaRepository<VisaApplication, Long> {
+//Add custom query methods
+    List<VisaApplication> findByApplicantId(Long applicantId);
+    List<VisaApplication> findByStatus(String status);
 }
