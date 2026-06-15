63abe4172876756c18264e432562f550db963086
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 15:25:00 +0400

    add(ImmigrationCenter):  declaring all fields.

--- src/main/java/com/immigration/system/entities/ImmigrationCenter.java
+++ src/main/java/com/immigration/system/entities/ImmigrationCenter.java
@@ -1,8 +1,20 @@
 package com.immigration.system.entities;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 
 @Entity
 public class ImmigrationCenter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    //Declaring variables
+    private Long id;
+    private String name;
+    private String locationCountry;
+    private String type; // "Embassy", "Border"
+    private int dailyCapacity;
 
 }
