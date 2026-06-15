1d4771c30b90e602f7ab375bbee2a46a601fad99
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 15:03:41 +0400

    add(AsylumSeeker): declaring all fields & add constructor.

--- src/main/java/com/immigration/system/entities/AsylumSeeker.java
+++ src/main/java/com/immigration/system/entities/AsylumSeeker.java
@@ -2,5 +2,16 @@ package com.immigration.system.entities;
 import jakarta.persistence.Entity;
 
 @Entity
-public class AsylumSeeker extends Applicant{
+public class AsylumSeeker extends Applicant {
+
+    // Declaring Variables
+    private String countryOfOrigin;
+    private String sponsorOrganization;
+
+    //constructor
+    public AsylumSeeker(String countryOfOrigin, String sponsorOrganization) {
+        this.countryOfOrigin = countryOfOrigin;
+        this.sponsorOrganization = sponsorOrganization;
+    }
 }
+
