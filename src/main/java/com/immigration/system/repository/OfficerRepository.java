d06ff15d81d58187b88af2b292189104ce5a5041
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 16:12:51 +0400

    add(OfficerRepository):  implement custom query method "findByRank"..

--- src/main/java/com/immigration/system/repository/OfficerRepository.java
+++ src/main/java/com/immigration/system/repository/OfficerRepository.java
@@ -1,7 +1,11 @@
 package com.immigration.system.repository;
 
+import com.immigration.system.entities.ImmigrationOfficer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OfficerRepository extends JpaRepository {
+import java.util.List;
 
+public interface OfficerRepository extends JpaRepository {
+// Add custom query method
+    List<ImmigrationOfficer> findByRank(String rank);
 }
