2c343fbe788207c01668a621c4a872825603b482
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 15:40:49 +0400

    add(VisaApplication): implement getter & setter methods.

--- src/main/java/com/immigration/system/entities/VisaApplication.java
+++ src/main/java/com/immigration/system/entities/VisaApplication.java
@@ -18,4 +18,54 @@ public class VisaApplication {
     private String status; // "PENDING", "APPROVED", "REJECTED"
     private String officerNotes;
 
+    //Getter methods
+
+    public Long getId() {
+        return id;
+    }
+
+    public Applicant getApplicant() {
+        return applicant;
+    }
+
+    public ImmigrationOfficer getHandlingOfficer() {
+        return handlingOfficer;
+    }
+
+    public String getVisaType() {
+        return visaType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getOfficerNotes() {
+        return officerNotes;
+    }
+    // Setter method
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setApplicant(Applicant applicant) {
+        this.applicant = applicant;
+    }
+
+    public void setHandlingOfficer(ImmigrationOfficer handlingOfficer) {
+        this.handlingOfficer = handlingOfficer;
+    }
+
+    public void setVisaType(String visaType) {
+        this.visaType = visaType;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setOfficerNotes(String officerNotes) {
+        this.officerNotes = officerNotes;
+    }
 }
