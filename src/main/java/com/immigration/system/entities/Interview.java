9da275fc6659c2f320e6bed4961459030bc83e64
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 15:45:08 +0400

    add(Interview):  implement getter & setter methods.

--- src/main/java/com/immigration/system/entities/Interview.java
+++ src/main/java/com/immigration/system/entities/Interview.java
@@ -17,4 +17,57 @@ public class Interview {
     private String interviewDate; // "YYYY-MM-DD"
     private String status; // "SCHEDULED", "COMPLETED", "CANCELLED"
     private String purpose;
+
+    public Interview() {}
+
+    // Getter method
+
+    public Long getId() {
+        return id;
+    }
+
+    public Applicant getApplicant() {
+        return applicant;
+    }
+
+    public ImmigrationOfficer getOfficer() {
+        return officer;
+    }
+
+    public String getInterviewDate() {
+        return interviewDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+    //Setter Method
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setApplicant(Applicant applicant) {
+        this.applicant = applicant;
+    }
+
+    public void setOfficer(ImmigrationOfficer officer) {
+        this.officer = officer;
+    }
+
+    public void setInterviewDate(String interviewDate) {
+        this.interviewDate = interviewDate;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
 }
