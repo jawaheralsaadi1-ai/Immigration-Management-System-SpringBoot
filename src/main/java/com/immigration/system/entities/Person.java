351edbe0304be94b64e3c5bb924eeda5ae7dd5e9
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 13:41:01 +0400

    add: create person class

---
+++ src/main/java/com/immigration/system/entities/Person.java
@@ -0,0 +1,17 @@
+package com.immigration.system.entities;
+
+
+/**
+ * what will do ?
+ 1.Create an abstract class Person . (Do not use @Entity, only use  @MappedSuperclass so subclasses inherit these columns
+ * without Person itself becoming a database table.
+ * Fields:firstName (String),lastName (String),
+ gender (String), phoneNumber (String), email (String).
+
+ 2. Implement standard constructors, getters, setters, and tostring() .
+
+ */
+
+public class Person {
+
+}
