03dd596aca98f60459559419e4fffb3583d4f8dd
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 15:20:03 +0400

    add(BorderControlOfficer):declaring all fields with constructor.

--- src/main/java/com/immigration/system/entities/BorderControlOfficer.java
+++ src/main/java/com/immigration/system/entities/BorderControlOfficer.java
@@ -4,4 +4,14 @@ import jakarta.persistence.Entity;
 @Entity
 public class BorderControlOfficer extends ImmigrationOfficer {
 
+    //Declaring variables
+    private String assignedCheckpoint;
+    private boolean k9UnitAssigned;
+
+    //Constructor
+
+    public BorderControlOfficer(String assignedCheckpoint, boolean k9UnitAssigned) {
+        this.assignedCheckpoint = assignedCheckpoint;
+        this.k9UnitAssigned = k9UnitAssigned;
+    }
 }
