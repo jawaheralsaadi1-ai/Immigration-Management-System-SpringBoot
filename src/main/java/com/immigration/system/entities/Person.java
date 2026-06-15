d97737ed2da0950ac167a0fd9cb9c660f3f67bbb
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 14:15:34 +0400

    add(Person): implement standard getters & setters.

--- src/main/java/com/immigration/system/entities/Person.java
+++ src/main/java/com/immigration/system/entities/Person.java
@@ -33,5 +33,46 @@ import jakarta.persistence.MappedSuperclass;
         this.phoneNumber = phoneNumber;
         this.email       = email;
     }
+    //Getters
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    // Setters
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
