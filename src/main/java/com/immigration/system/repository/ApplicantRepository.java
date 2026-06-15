158eedbf04083025ef46d4b16828f6c8e5714ae6
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 16:07:10 +0400

    add(ApplicantRepository): implement custom query method "findByNationality".

--- src/main/java/com/immigration/system/repository/ApplicantRepository.java
+++ src/main/java/com/immigration/system/repository/ApplicantRepository.java
@@ -3,5 +3,9 @@ package com.immigration.system.repository;
 import com.immigration.system.entities.Applicant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
+    //Add custom query methods
+    List<Applicant> findByNationality(String nationality);
 }
