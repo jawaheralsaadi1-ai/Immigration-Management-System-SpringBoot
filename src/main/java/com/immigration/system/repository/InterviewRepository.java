94c2f55d6465c7d11ff3cb63006149aa6f56d5e9
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 16:16:08 +0400

    add(InterviewRepository): implement custom query method.

--- src/main/java/com/immigration/system/repository/InterviewRepository.java
+++ src/main/java/com/immigration/system/repository/InterviewRepository.java
@@ -3,5 +3,10 @@ package com.immigration.system.repository;
 import com.immigration.system.entities.Interview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InterviewRepository extends JpaRepository<Interview, Long> {
+
+    //Add custom query methods
+    List<Interview> findByOfficerIdAndInterviewDate(Long officerId, String date);
 }
