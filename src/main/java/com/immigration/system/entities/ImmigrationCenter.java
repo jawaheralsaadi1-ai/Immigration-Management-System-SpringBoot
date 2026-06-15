ec10730fabe5ea66d4eb2e4168371f580ddd9068
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 15:32:42 +0400

    add(ImmigrationCenter):  implement getter & setter methods.

--- src/main/java/com/immigration/system/entities/ImmigrationCenter.java
+++ src/main/java/com/immigration/system/entities/ImmigrationCenter.java
@@ -20,4 +20,56 @@ public class ImmigrationCenter {
     //Officers (@OneToMany ) .
     @OneToMany(mappedBy = "center")
     private List<ImmigrationOfficer> officers = new ArrayList<>();
+
+// Getter method
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocationCountry() {
+        return locationCountry;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getDailyCapacity() {
+        return dailyCapacity;
+    }
+
+    public List<ImmigrationOfficer> getOfficers() {
+        return officers;
+    }
+
+    //setter method
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocationCountry(String locationCountry) {
+        this.locationCountry = locationCountry;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDailyCapacity(int dailyCapacity) {
+        this.dailyCapacity = dailyCapacity;
+    }
+
+    public void setOfficers(List<ImmigrationOfficer> officers) {
+        this.officers = officers;
+    }
 }
