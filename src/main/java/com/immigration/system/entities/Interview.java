ee79eca366e48bb7e53f6b9930b8e2347c517536
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 15:43:43 +0400

    add(Interview): declaring all fields.

--- src/main/java/com/immigration/system/entities/Interview.java
+++ src/main/java/com/immigration/system/entities/Interview.java
@@ -1,8 +1,20 @@
 package com.immigration.system.entities;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 
 @Entity
 public class Interview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "applicant_id")
+    private Applicant applicant;
+    @ManyToOne @JoinColumn(name = "officer_id")
 
+    //Declaring Variables
+    private ImmigrationOfficer officer;
+    private String interviewDate; // "YYYY-MM-DD"
+    private String status; // "SCHEDULED", "COMPLETED", "CANCELLED"
+    private String purpose;
 }
