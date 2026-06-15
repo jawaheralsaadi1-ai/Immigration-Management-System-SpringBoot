cef26328326348bcaad9feacb6a33d9a463822d8
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 15:38:31 +0400

    add(VisaApplication): Declaring all fields.

--- src/main/java/com/immigration/system/entities/VisaApplication.java
+++ src/main/java/com/immigration/system/entities/VisaApplication.java
@@ -1,6 +1,21 @@
 package com.immigration.system.entities;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class VisaApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "applicant_id")
+    private Applicant applicant;
+    @ManyToOne
+    @JoinColumn(name = "officer_id")
+
+    //Declaring Variables
+    private ImmigrationOfficer handlingOfficer;
+    private String visaType;
+    private String status; // "PENDING", "APPROVED", "REJECTED"
+    private String officerNotes;
+
 }
