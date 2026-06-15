7539a347dd55cd06ce3caca1db9f3e5c499e7bb6
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-15 15:13:28 +0400

    add(ImmigrationOfficer): implement Primary Key & declaring Fields.

--- src/main/java/com/immigration/system/entities/ImmigrationOfficer.java
+++ src/main/java/com/immigration/system/entities/ImmigrationOfficer.java
@@ -1,6 +1,18 @@
 package com.immigration.system.entities;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class ImmigrationOfficer extends  Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    //Declaring variables
+    private Long id;
+    private String badgeNumber;
+    private String rank;
+    private int clearanceLevel;
+    private boolean active;
 }
