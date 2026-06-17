07eb3fc8f5c3d1eb0278739edad7561c558429ec
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-17 15:08:38 +0400

    add(CenterRequest):implement DTO & getters/setters methods.

--- src/main/java/com/immigration/system/dto/CenterRequest.java
+++ src/main/java/com/immigration/system/dto/CenterRequest.java
@@ -1,4 +1,27 @@
 package com.immigration.system.dto;
 
+
 public class CenterRequest {
+
+    //Declaring variables
+    private String name;
+    private String locationCountry;
+    private String type;
+    private int dailyCapacity;
+
+    public CenterRequest() {}
+
+
+    //Setter & getter methods
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getLocationCountry() { return locationCountry; }
+    public void setLocationCountry(String locationCountry) { this.locationCountry = locationCountry; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public int getDailyCapacity() { return dailyCapacity; }
+    public void setDailyCapacity(int dailyCapacity) { this.dailyCapacity = dailyCapacity; }
 }
