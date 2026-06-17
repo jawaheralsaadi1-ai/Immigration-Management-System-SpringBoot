0131910036e4dca8df6beed32aaa0e539491a9e6
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-17 13:56:04 +0400

    add(service): create ApplicantService class with @Service.

---
+++ src/main/java/com/immigration/system/service/ApplicantService.java
@@ -0,0 +1,10 @@
+package com.immigration.system.service;
+import org.springframework.stereotype.Service;
+
+/**
+ * Service handling all business logic for Applicants.
+
+ */
+@Service
+public class ApplicantService {
+}
