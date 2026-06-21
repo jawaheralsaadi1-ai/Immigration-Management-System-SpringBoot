034bd169f03f1b82a31061e55f7ab4e3c6d382f3
Jawaher <jawaheralsaadi1@gmail.com>
2026-06-21 14:04:27 +0400

    fix(entities) : AsylumSeeker class.

--- src/main/java/com/immigration/system/entities/AsylumSeeker.java
+++ src/main/java/com/immigration/system/entities/AsylumSeeker.java
@@ -1,6 +1,8 @@
 package com.immigration.system.entities;
 import jakarta.persistence.Entity;
 
+import java.util.List;
+
 @Entity
 public class AsylumSeeker extends Applicant {
 
@@ -9,7 +11,21 @@ public class AsylumSeeker extends Applicant {
     private String sponsorOrganization;
 
     //constructor
+    public AsylumSeeker() {}
+
     public AsylumSeeker(String countryOfOrigin, String sponsorOrganization) {
+        this.countryOfOrigin = countryOfOrigin;
+        this.sponsorOrganization = sponsorOrganization;
+    }
+
+    public AsylumSeeker(Long id, String passportNumber, String nationality, boolean criminalRecord, List<VisaApplication> visaApplications, List<Interview> interviews, String countryOfOrigin, String sponsorOrganization) {
+        super(id, passportNumber, nationality, criminalRecord, visaApplications, interviews);
+        this.countryOfOrigin = countryOfOrigin;
+        this.sponsorOrganization = sponsorOrganization;
+    }
+
+    public AsylumSeeker(String firstName, String lastName, String gender, String phoneNumber, String email, Long id, String passportNumber, String nationality, boolean criminalRecord, List<VisaApplication> visaApplications, List<Interview> interviews, String countryOfOrigin, String sponsorOrganization) {
+        super(firstName, lastName, gender, phoneNumber, email, id, passportNumber, nationality, criminalRecord, visaApplications, interviews);
         this.countryOfOrigin = countryOfOrigin;
         this.sponsorOrganization = sponsorOrganization;
     }
